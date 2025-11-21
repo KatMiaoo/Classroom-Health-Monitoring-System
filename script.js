aa507f679c42de5b52e1101e2c9045b7a762ac09
joberth <joberthpalomera18@gmail.com>
2025-11-22 04:00:40 +0800

    Added addStudent Fucntion "name and temp"

---
+++ script.js
@@ -0,0 +1,14 @@
+let editIndex = null;
+
+function addStudent() {
+    let name = document.getElementById("studentName").value;
+    let temp = document.getElementById("temperature").value;
+
+    let sicknessList = [];
+    let checkboxes = document.getElementById("sickOption");
+
+    
+
+
+
+}
