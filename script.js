9ad7f48d74342594790f272e61da684b75f23586
joberth <joberthpalomera18@gmail.com>
2025-11-23 23:11:14 +0800

    Added surname, first name, Middle initial;

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let editIndex = null;
 
 function addStudent() {
-    let name = document.getElementById("studentName").value;
+    let surname = document.getElementById("surname").value;
+    let firstName = document.getElementById("firstName").value;
+    let mi = document.getElementById("MI").value;
     let temp = document.getElementById("temperature").value;
 
     let sicknessList = [];
@@ -16,7 +18,7 @@ function addStudent() {
     let manual = document.getElementById("manualSickness").value;
     if (manual !== "") sicknessList.push(manual);
 
-    if (name === "" || temp === "") {
+    if (surname === "" || firstName === "" || mi === "" || temp === "") {
         alert("Please fill all fields!");
         return;
     }
@@ -31,18 +33,22 @@ function addStudent() {
 
     if (editIndex === null) {
         let row = table.insertRow();
-        row.insertCell(0).innerHTML = name;
-        row.insertCell(1).innerHTML = temp;
-        row.insertCell(2).innerHTML = sicknessText;
-        row.insertCell(3).innerHTML = `
+        row.insertCell(0).innerHTML = surname;
+        row.insertCell(1).innerHTML = firstName;
+        row.insertCell(2).innerHTML = mi;
+        row.insertCell(3).innerHTML = temp;
+        row.insertCell(4).innerHTML = sicknessText;
+        row.insertCell(5).innerHTML = `
             <button class='btn-edit' onclick='editStudent(this)'>Edit</button>
             <button class='btn-delete' onclick='deleteStudent(this)'>Delete</button>
         `;
     } else {
         let row = table.rows[editIndex];
-        row.cells[0].innerHTML = name;
-        row.cells[1].innerHTML = temp;
-        row.cells[2].innerHTML = sicknessText;
+        row.cells[0].innerHTML = surname;
+        row.cells[1].innerHTML = firstName;
+        row.cells[2].innerHTML = mi;
+        row.cells[3].innerHTML = temp;
+        row.cells[4].innerHTML = sicknessText;
         editIndex = null;
     }
 
@@ -53,10 +59,12 @@ function editStudent(button) {
     let row = button.parentNode.parentNode;
     editIndex = row.rowIndex - 1;
 
-    document.getElementById("studentName").value = row.cells[0].innerHTML;
-    document.getElementById("temperature").value = row.cells[1].innerHTML;
+    document.getElementById("surname").value = row.cells[0].innerHTML;
+    document.getElementById("firstName").value = row.cells[1].innerHTML;
+    document.getElementById("MI").value = row.cells[2].innerHTML;
+    document.getElementById("temperature").value = row.cells[3].innerHTML;
 
-    let sicknesses = row.cells[2].innerHTML.split(", ");
+    let sicknesses = row.cells[4].innerHTML.split(", ");
 
     let checkboxes = document.getElementsByClassName("sickOption");
     for (let box of checkboxes) box.checked = false;
@@ -78,7 +86,9 @@ function deleteStudent(button) {
 }
 
 function clearInputs() {
-    document.getElementById("studentName").value = "";
+    document.getElementById("surname").value = "";
+    document.getElementById("firstName").value = "";
+    document.getElementById("MI").value = "";
     document.getElementById("temperature").value = "";
     document.getElementById("manualSickness").value = "";
 
